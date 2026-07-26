ba1f4ee17f79c39f20cb89c71c9751f5796cc528
adrianna <adrianna@ravenhood.net>
2026-07-26 11:24:17 -0400

    advance version #

--- game/release_runtime.py
+++ game/release_runtime.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from engine.persistence import SAVE_DIR
 
 
-GAME_VERSION = "0.5.02-theunnamedrelease"
+GAME_VERSION = "0.5.03-isaregisteredtrademark"
 CRASH_REPORT_PATH = SAVE_DIR / "bakerrrr_last_crash.txt"
 _SIGUSR2_DEBUG_UNLOCKED = False
 _ACTIVE_DEBUG_SIM = None
